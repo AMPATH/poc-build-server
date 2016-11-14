1968fbdf55971e8a7cc1927ca200c9f77b7b56dc
Emmanuel Nyachoke <achachiez@gmail.com>
2016-11-14 11:08:30 +0300

    Add clone logic

--- build.js
+++ build.js
@@ -11,7 +11,7 @@ const appDir = config.buildDir + config.appName;
 const appDeployDir = config.deployDir + config.appName;
 fs.exists(appDir, function(exists) {
     if (exists) {
-      let spawn = require('child_process').spawn;
+        let spawn = require('child_process').spawn;
         simpleGit(config.buildDir + config.appName)
             .pull(function(err, update) {
                 let ls = spawn('npm', ['install'], {
@@ -42,20 +42,27 @@ fs.exists(appDir, function(exists) {
                         });
 
                         build.on('exit', function(code) {
-                          if (code === 0) {
-                            fse.ensureLink(appDir +'/dist', appDeployDir, function (err) {
-                              console.log(err) // => null
-                              // link has now been created, including the directory it is to be placed in
-                            })
-                          }else{
-                            console.log('build failed maybe another time');
-                          }
+                            if (code === 0) {
+                                fse.ensureLink(appDir + '/dist', appDeployDir, function(err) {
+                                    console.log(err) // => null
+                                        // link has now been created, including the directory it is to be placed in
+                                })
+                            } else {
+                                console.log('build failed maybe another time');
+                            }
 
                         });
                     }
 
                 });
             });
+    } else {
+        simpleGit().clone(config.gitRepo, config.buildDir + config.appName, function(err, data) {
+            if (err) {
+                console.log('Error', err);
+            }
+            console.log('Clone Done');
+        });
     }
 });
 // utils.checkDirectory(config + config.appName, function(status) {
